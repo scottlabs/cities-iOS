c95d561dd89174ca64821359cd1043663288334d
Kevin Scott <kevin@scottlabsllc.com>
2012-11-13 15:12:42 -0500

    Initial Commit

---
+++ CitiesTests/CitiesTests.h
@@ -0,0 +1,13 @@
+//
+//  CitiesTests.h
+//  CitiesTests
+//
+//  Created by Kevin Scott on 11/13/12.
+//  Copyright (c) 2012 Kevin Scott. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CitiesTests : SenTestCase
+
+@end
---
+++ CitiesTests/CitiesTests.m
@@ -0,0 +1,32 @@
+//
+//  CitiesTests.m
+//  CitiesTests
+//
+//  Created by Kevin Scott on 11/13/12.
+//  Copyright (c) 2012 Kevin Scott. All rights reserved.
+//
+
+#import "CitiesTests.h"
+
+@implementation CitiesTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CitiesTests");
+}
+
+@end
